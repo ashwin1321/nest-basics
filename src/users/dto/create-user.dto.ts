7acf2361f5c10c69dc7e993d139e87b1399f71e4
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-15 11:58:10 +0545

    feat: adding required porperty in dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
 // Dto stands for Data Transfer Object. It is the schema representation of the data that is being transferred between the client and the server.
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
-  name: string;
+  @ApiProperty()
+  name: string; // required property
+
+  @ApiProperty({ required: false }) // optional property
+  age?: number;
 }
