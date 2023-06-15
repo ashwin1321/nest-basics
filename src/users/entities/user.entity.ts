c24298c04adb329b402d58cd55d787bda5406910
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-15 12:07:19 +0545

    feat: Use api proptery in swagger doc

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,11 @@
 // this is class that represent the shape of the object that we want to store in the database
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class User {
+  @ApiProperty() // we can use the ApiProperty decorator to add a property to the Swagger document
   id: number;
+
+  @ApiProperty()
   name: string;
 }
