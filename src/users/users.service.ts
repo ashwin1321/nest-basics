6b2df9a74c3271adeb252443571f5ac50e6de8b6
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-15 12:17:01 +0545

    refactor: code refactor to check the name query

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,9 +4,16 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [{ id: 1, name: 'Ashwin' }];
+  private users: User[] = [
+    { id: 1, name: 'Ashwin' },
+    { id: 0, name: 'Ashwin' },
+    { id: 4, name: 'test' },
+  ];
 
-  findAll(): User[] {
+  findAll(name?: string): User[] {
+    if (name) {
+      return this.users.filter((user) => user.name === name);
+    }
     return this.users;
   }
 
